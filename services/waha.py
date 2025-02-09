59bc077d30ce6e265555cd28e12d4fa2a7f8fce3
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-09 15:30:40 -0300

    feat: added start_typing and stop_typing in waha.py

--- services/waha.py
+++ services/waha.py
@@ -14,16 +14,6 @@ class Waha:
     def __init__(self):
         self.__api_url = 'http://waha:3000'
 
-    def generate_custom_message(self, message: str) -> str:
-        model = genai.GenerativeModel("gemini-1.5-flash") 
-    
-        response = model.generate_content( 
-            message, 
-            generation_config = genai.GenerationConfig(max_output_tokens=300,) 
-        ) 
-
-        return str(response.text)
-
     def send_message(self, chat_id, message):
         url = f'{self.__api_url}/api/sendText'
         headers = {
@@ -52,3 +42,49 @@ class Waha:
         except requests.exceptions.RequestException as e:
             print(f"Erro ao enviar mensagem: {e}")
             return None
+    
+    def start_typing(self, chat_id):
+        url = f'{self.__api_url}/api/startTyping'
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        payload = {
+            'session': 'default',
+            'chatId': chat_id
+        }
+
+        requests.post(
+            url=url,
+            json=payload,
+            headers=headers
+        )
+
+    def stop_typing(self, chat_id):
+        url = f'{self.__api_url}/api/stopTyping'
+        
+        headers = {
+            'Content-Type': 'application/json'
+        }
+
+        payload = {
+            'session': 'default',
+            'chatId': chat_id
+        }
+
+        requests.post(
+            url=url,
+            json=payload,
+            headers=headers
+        )
+
+    def generate_custom_message(self, message: str) -> str:
+        model = genai.GenerativeModel("gemini-1.5-flash") 
+    
+        response = model.generate_content( 
+            message, 
+            generation_config = genai.GenerationConfig(max_output_tokens=300,) 
+        ) 
+
+        return str(response.text)
