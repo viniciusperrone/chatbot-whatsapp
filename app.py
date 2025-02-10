ae397e4798361ff386f683a255a4e094d747ee05
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-09 23:59:00 -0300

    feat: created rag.py

--- app.py
+++ app.py
@@ -18,18 +18,24 @@ def webhook():
     chat_id = data['payload']['from']
     received_message = data['payload']['body']
 
-    waha.start_typing(chat_id=chat_id)
+    is_group = '@g.us' in chat_id
+    is_status = 'status@broadcast' in chat_id
 
-    time.sleep(4)
+    if is_group or is_status:
+        return jsonify({ 'status': 'success', 'message': 'Mensagem de grupo/status ignorado.'})
 
-    waha.stop_typing(chat_id=chat_id)
+    # waha.start_typing(chat_id=chat_id)
 
-    response = ai_bot.invoke(question=received_message)
+    # time.sleep(4)
 
-    waha.send_message(
-        chat_id=chat_id,
-        message=response
-    )
+    # waha.stop_typing(chat_id=chat_id)
+
+    # response = ai_bot.invoke(question=received_message)
+
+    # waha.send_message(
+    #     chat_id=chat_id,
+    #     message=response
+    # )
 
     return jsonify({ 'status': 'success'}), 200 
 
---
+++ rag/data/rag.py
@@ -0,0 +1,36 @@
+import os 
+
+from decouple import config
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_huggingface import HuggingFaceEmbeddings
+
+os.environ['HUGGINGFACE_API_KEY'] = config('HUGGINGFACE_API_KEY')
+
+if __name__ == 'main':
+    file_path = '/app/rag/data/django_master.pdf'
+    loader = PyPDFLoader(file_path)
+    docs = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = text_splitter.split_documents(
+        documents=docs
+    )
+
+    persist_directory = '/app/chroma_data'
+
+    embedding = HuggingFaceEmbeddings()
+    vector_store = Chroma(
+        embedding_function=embedding,
+        persist_directory=persist_directory
+    )
+
+    
+
+
