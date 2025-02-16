e6c19b92975d2fbecc561fe93b3e60b388adb2ec
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-02-16 10:34:08 -0300

    fix: improving rag.py

--- rag/rag.py
+++ rag/rag.py
@@ -7,29 +7,37 @@ from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_huggingface import HuggingFaceEmbeddings
 
+os.environ['GROQ_API_KEY'] = config('GROQ_API_KEY')
 os.environ['HUGGINGFACE_API_KEY'] = config('HUGGINGFACE_API_KEY')
 
-if __name__ == 'main':
-    file_path = '/app/rag/data/CV_VINICIUS_PERRONE_2024.pdf'
-    loader = PyPDFLoader(file_path)
-    docs = loader.load()
+def exec():
+    try:
+        file_path = '/app/rag/data/CV_VINICIUS_PERRONE_2024.pdf'
+        loader = PyPDFLoader(file_path)
+        docs = loader.load()
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=1000,
-        chunk_overlap=200
-    )
+        text_splitter = RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200
+        )
 
-    chunks = text_splitter.split_documents(
-        documents=docs
-    )
+        chunks = text_splitter.split_documents(
+            documents=docs
+        )
 
-    persist_directory = '/app/chroma_data'
+        persist_directory = '/app/chroma_data'
 
-    embedding = HuggingFaceEmbeddings()
-    vector_store = Chroma(
-        embedding_function=embedding,
-        persist_directory=persist_directory
-    )
-    vector_store.add_documents(
-        documents=chunks
-    )
+        embedding = HuggingFaceEmbeddings()
+        vector_store = Chroma(
+            embedding_function=embedding,
+            persist_directory=persist_directory
+        )
+        vector_store.add_documents(
+            documents=chunks
+        )
+        
+    except Exception as e:
+        print(f"Error: {e}")
+
+if __name__ == '__main__':
+    exec()
